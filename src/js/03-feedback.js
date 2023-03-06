1dea99838b439b0286719061e3cdfca3991b3f88
AntonVlasenkoo <antonvlasenko2005@gmail.com>
2023-03-06 22:42:11 +0200

    додає код завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,54 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
+
+const STORAGE_KEY = 'feedback-form-state'
+const formData = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+// ======================================================
+function onFormInput(e) {
+formData[e.target.name] = e.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+};
+
+// ======================================================
+function populeteForm() {
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+const parseSavedData = JSON.parse(savedData);
+
+if (savedData) {
+    input.value = parseSavedData.email;
+    textarea.value = parseSavedData.message;
+}
+};
+populeteForm();
+
+
+// ======================================================
+function onFormSubmit(e) {
+
+    e.preventDefault();
+
+    e.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY)
+
+};
+
+
+
+
+
+
+
 
 
 
