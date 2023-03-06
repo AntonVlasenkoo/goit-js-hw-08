a48f5b24b56a1e6ef6e39038cd92b456949fb79c
AntonVlasenkoo <113206626+AntonVlasenkoo@users.noreply.github.com>
2023-03-06 19:15:02 +0000

    Deploying to gh-pages from @ AntonVlasenkoo/goit-js-hw-08@3633a5c6ef6093ec06829340cd77fc33cc1d28ba 🚀

---
+++ 03-feedback.b47c756a.js
@@ -0,0 +1,2 @@
+document.querySelector("input"),document.querySelector("textarea");
+//# sourceMappingURL=03-feedback.b47c756a.js.map
---
+++ 03-feedback.eb8d1ee5.js
@@ -0,0 +1,2 @@
+document.querySelector("input"),document.querySelector("textarea");
+//# sourceMappingURL=03-feedback.eb8d1ee5.js.map
