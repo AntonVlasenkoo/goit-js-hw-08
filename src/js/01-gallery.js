2639df95b66f0035e85387b02fbcdd14d3c78d1b
AntonVlasenkoo <antonvlasenko2005@gmail.com>
2023-03-06 18:42:31 +0200

    додає розмітку другого завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,68 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// // Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
 
 console.log(galleryItems);
 
-
-
 const galleryContainer = document.querySelector(".gallery");
 
-
+//=====================================================================
 function createImageCardMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
- <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-            />
-        </a>
- </div> 
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview} " alt="${description}" title="${description}"/>
+</a> 
+
  `;
     }).join(' ');
 };
+//=====================================================================
+
 
 
 const cardMarkup = createImageCardMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', cardMarkup)
-
-galleryContainer.addEventListener('click', onImageClick)
-
-function onImageClick(event) {
-   
-    blockStandardAction(event);
-
-
-    console.log(event.target.dataset);
-   if (event.target.nodeName !== "IMG") {
-       return;
-    };
-   
-
-
-const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-
-    instance.show();
-
-    galleryContainer.addEventListener('keydown', (event) => {
-        if (event.code === "Escape") {
-        instance.close()
-    }
-    });
-
-
-};
-
-function blockStandardAction(event) {
-    event.preventDefault();
-};
-
-
+    var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle";
+import Player  from '@vimeo/player';
+//=========================================
+const STORAGE_KEY = 'videoplayer-current-time'
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    const onPlay = function(time) {
+localStorage.setItem(STORAGE_KEY, JSON.stringify(time));
+};
+player.on('timeupdate', throttle((onPlay), 1000));  
+
+const times = localStorage.getItem(STORAGE_KEY);
+const parseTimes = JSON.parse(times);
+console.log(parseTimes.seconds);
+
+player.setCurrentTime(parseTimes.seconds).then(function(seconds) {
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+    
+            break;
+
+        default:
+          
+            break;
+    }
+});
