0b6038190f86250519012e57aacd1a91eb808374
AntonVlasenkoo <antonvlasenko2005@gmail.com>
2023-03-06 23:04:37 +0200

    додає перевірку інпуту та тексттерії

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,8 +8,8 @@ const STORAGE_KEY = 'feedback-form-state'
 const formData = {};
 
 form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onСheckЕheSubmiForm);
 form.addEventListener('submit', onFormSubmit);
-
 // ======================================================
 function onFormInput(e) {
 formData[e.target.name] = e.target.value;
@@ -38,10 +38,24 @@ function onFormSubmit(e) {
     e.preventDefault();
 
     e.currentTarget.reset();
-
+  console.log(formData);
     localStorage.removeItem(STORAGE_KEY)
+  
+};
+
+
+function onСheckЕheSubmiForm(event) {
+    event.preventDefault();
+  
+    const formElements = event.currentTarget.elements;
+
+    
+if (formElements.email.value === "" || formElements.message.value === "") {
+    return alert("Bсі поля повинні бути заповнені!!!");
+    };
 
 };
+
 
 
 
